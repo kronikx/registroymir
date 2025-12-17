60a5cc1b49ea947e397d6bafc70693fb67648876
kronikx <kronikxtaiwan@gmail.com>
2025-12-17 15:03:18 +0100

    Delete registroymir directory

--- registroymir/api/discordAuth/check.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const cookie = req.headers.cookie || "";
-  const userId = cookie.split("discordUser=")[1];
-  if (userId) {
-    return res.json({ ok: true, userId });
-  } else {
-    return res.json({ ok: false });
-  }
-}
